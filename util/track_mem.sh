a15925628af9750b620513e1db9f1da379538ea8
Joris.Roels <jorisro@cn1385.irc.ugent.be>
2021-07-15 16:58:10 +0200

    track GPU memory

--- util/track_mem.sh
+++ util/track_mem.sh
@@ -7,14 +7,21 @@
 pid=$1
 
 max_mem=0
+max_mem_gpu=0
 
 while ps --pid $pid &>/dev/null
 do
     mem=$(ps -p $pid -o pmem=)
+    mem_gpu=$(gpustat -p | grep $pid | cut -d '|' -f3 | cut -d '/' -f1)
     if (($(echo "$mem > $max_mem" | bc -l)))
     then
         max_mem=$mem
     fi
+    if [[ $mem_gpu -gt $max_mem_gpu ]]
+    then
+        max_mem_gpu=$mem_gpu
+    fi
 done
 
 echo "Maximum (resilient) memory usage:$max_mem GB"
+echo "Maximum GPU memory usage:$max_mem_gpu MB"
