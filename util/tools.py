ca874e03a26d626c155ec21dce4a1ad6974ad108
jorisroels <joris.roels@ugent.vib.be>
2021-09-29 15:44:48 +0200

    cleaner code

--- util/tools.py
+++ util/tools.py
@@ -8,11 +8,40 @@ from neuralnets.util.io import print_frm
 from neuralnets.util.augmentation import *
 
 
+def get_transforms(tfs, coi=None):
+    """
+    Builds a transform object based on a list of desired augmentations
+
+    :param tfs: list of augmentations, options: rot90, flipx, flipy, contrast, deformation, noise
+    :param coi: classes of interest (only required if deformations are included)
+    :return: transform object that implements the desired augmentations
+    """
+
+    # dictionary that maps augmentation strings to transform objects
+    mapper = {'rot90': Rotate90(),
+              'flipx': Flip(prob=0.5, dim=0),
+              'flipy': Flip(prob=0.5, dim=1),
+              'contrast': ContrastAdjust(adj=0.1),
+              'deformation': RandomDeformation(),
+              'noise': AddNoise(sigma_max=0.05)}
+
+    # build the transforms
+    tf_list = []
+    for key in mapper:
+        if key in tfs:
+            tf_list.append(mapper[key])
+
+    # required post-processing
+    if 'deformation' in tfs:
+        tf_list.append(CleanDeformedLabels(coi))
+
+    return Compose(tf_list)
+
+
 def get_dataloaders(params, domain=None, domain_labels_available=1.0, supervised=False):
 
     input_shape = (1, *(params['input_size']))
-    transform = Compose([Rotate90(), Flip(prob=0.5, dim=0), Flip(prob=0.5, dim=1), ContrastAdjust(adj=0.1),
-                         AddNoise(sigma_max=0.05)])
+    transform = get_transforms(params['augmentation'])
     len_epoch = 2000
 
     if domain is None:
@@ -86,6 +115,16 @@ def parse_params(params):
 
     if 'tar_labels_available' in keys:
         params['tar_labels_available'] = float(params['tar_labels_available'])
+
+    if 'len_epoch' in keys:
+        params['len_epoch'] = int(params['len_epoch'])
+    else:
+        params['len_epoch'] = 2000
+
+    if 'augmentation' in keys:
+        params['augmentation'] = params['augmentation'].split(',')
+    else:
+        params['augmentation'] = 'rot90,flipx,flipy,contrast,noise'.split(',')
 
     for dom in ['src', 'tar']:
         if dom in keys:
