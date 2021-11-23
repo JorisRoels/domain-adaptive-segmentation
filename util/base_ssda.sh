1e362d237b7c70dfa1dbdb4557e9802042620a55
jorisroels <joris.roels@ugent.vib.be>
2021-11-23 12:14:50 +0100

    script update

--- util/base_ssda.sh
+++ util/base_ssda.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -l
 #PBS -l nodes=1:ppn=8:gpus=1
 #PBS -l mem=32gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=28:00:00
+module purge
+module load PyTorch
+module unload PyTorch
 
 # define context variables
-PYTHON_EXE=$PYTHON_DEFAULT
+PYTHON_EXE=python
 
 # define environment variables
 PROJECT_DIR=$HOME/research/domain-adaptive-segmentation
-export PYTHONPATH=$PYTHONPATH:$HOME/research/lib/python:$HOME/research/neuralnets:$PROJECT_DIR
+export PYTHONPATH=$HOME/research/neuralnets:$HOME/research/lib/python:$PROJECT_DIR
+LOG_DIR=ssda
 
 # DOMAINS=("EPFL" "UroCell" "po936q" "MitoEM-H" "MitoEM-R" "MiRA" "Kasthuri" "VNC" "EMBL" "evhela")
 DOMAINS_SRC=(<SRC_DOMAINS>)
@@ -18,7 +22,7 @@ METHOD=<METHOD>
 
 AVAILABLE_LABELS=<AVAILABLE_LABELS>
 
-COI=1
+COI=<COI>
 
 # number of experiments
 N=${#DOMAINS_SRC[@]}
@@ -32,8 +36,8 @@ do
         if [[ ${DOMAINS_SRC[$i]} != ${DOMAINS_TAR[$j]} ]]
         then
             CONFIG_FILE=${METHOD}-${AVAILABLE_LABELS}-${DOMAINS_SRC[$i]}2${DOMAINS_TAR[$j]}
-	        python $PROJECT_DIR/util/build_config_da.py -b $PROJECT_DIR/train/semi-supervised-da/config/base.yaml -ds ${DOMAINS_SRC[$i]} -dt ${DOMAINS_TAR[$j]} -m $METHOD -al $AVAILABLE_LABELS -c $COI -g 0
-	        python $PROJECT_DIR/train/train_semi_supervised.py -c $PROJECT_DIR/train/semi-supervised-da/config/${CONFIG_FILE}.yaml --clean-up &> $PROJECT_DIR/train/semi-supervised-da/logs/${CONFIG_FILE}.logs
+	          $PYTHON_EXE $PROJECT_DIR/util/build_config_da.py -b $PROJECT_DIR/train/semi-supervised-da/config/base.yaml -ds ${DOMAINS_SRC[$i]} -dt ${DOMAINS_TAR[$j]} -m $METHOD -al $AVAILABLE_LABELS -c $COI -g 0
+	          $PYTHON_EXE $PROJECT_DIR/train/train_semi_supervised.py -c $PROJECT_DIR/train/semi-supervised-da/config/${CONFIG_FILE}.yaml --clean-up &> $PROJECT_DIR/train/semi-supervised-da/logs/${CONFIG_FILE}.logs
         fi
     done
 done
