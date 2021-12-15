7724391f638e81c9e1c0fc7b9bd409c46c6d8c96
jorisroels <joris.roels@ugent.vib.be>
2021-12-15 15:20:28 +0100

    bug fix

--- util/tools.py
+++ util/tools.py
@@ -91,7 +91,7 @@ def get_dataloaders(params, domain=None, domain_labels_available=1.0, supervised
                                    in_channels=params['in_channels'], type=params['type'],
                                    batch_size=params['test_batch_size'], transform=transform,
                                    range_split=(split[0], split[1]), range_dir=range_dir, coi=params['coi'],
-                                   partial_labels=(1, params['tar_labels_available']), seed=params['seed'])
+                                   partial_labels=domain_labels_available, seed=params['seed'])
         print_frm('Test data...')
         test = LabeledSlidingWindowDataset(data, labels, in_channels=params['in_channels'], type=params['type'],
                                            batch_size=params['test_batch_size'], transform=transform,
